c9be6a1d2898d8bfffa71e6844b48f67909ce559
schleichardt <michael.schleichardt@commercetools.de>
2016-02-05 16:31:04 +0100

    Increase timeout and decrease log level.

--- src/test/java/QueryPublisherVerificationTest.java
+++ src/test/java/QueryPublisherVerificationTest.java
@@ -34,7 +34,7 @@ public class QueryPublisherVerificationTest extends PublisherVerification<Produc
     private static BlockingSphereClient client;
 
     public QueryPublisherVerificationTest() {
-        super(new TestEnvironment(300));
+        super(new TestEnvironment(500));
     }
 
     @BeforeClass
